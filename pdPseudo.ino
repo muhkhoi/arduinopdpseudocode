3bdd80a65061add2ed7c8a66b48554516f4379e0
muhkhoi <muhkhoi@icloud.com>
2019-05-14 15:50:37 +0700

    Add files via upload

---
+++ pdPseudo.ino
@@ -0,0 +1,35 @@
+int destDeg = 0, refDeg = 0;
+int kp = 1, kd = 1;
+int error, lastErr, mtrSpd;
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  refDeg = bacaRotary();                              //baca rotary degree
+  error = destDeg - refDeg;                           //hitung error
+  mtrSpd = (kp * error) + ((error - lastErr) * kd);   //kalkulasi pd
+  setSpd(mtrSpd);                                     //set aktuator
+  lastErr = error;                                    //simpan nilai sebelum
+}
+
+void serialEvent() {
+  //baca nilai sudut dari serial
+  while (Serial.available()) {
+    int destDeg = Serial.parseInt();
+    Serial.println(destDeg);
+  }
+}
+void setSpd(int pwmPulse)
+{
+  //set aktuator motor
+  //analogWrite(pwmPin, pwmPulse);
+}
+int bacaRotary()
+{
+  //kode konversi rotary
+  int derajat = 0;
+  return derajat;
+}
+
